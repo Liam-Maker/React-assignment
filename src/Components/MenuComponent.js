f79781babfe481487d0f14ce616cc13b8807f55c
Liam-Maker <liamthompson504@gmail.com>
2021-05-06 11:32:42 -0500

    more additions. trying to get it to render on browser.

--- src/Components/MenuComponent.js
+++ src/Components/MenuComponent.js
@@ -12,6 +12,29 @@ class Menu extends Component{
         }
     }
 
+    onDishSelect(dish){
+        this.setState({ selectedDish: dish});
+    }
+
+    renderDish(dish) {
+        if (dish != null){
+            return(
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        else{
+            return(
+                <div></div>
+            );
+        }
+    }
+
     render() {
 
         const menu = this.props.dishes.map((dish) =>{
@@ -39,9 +62,10 @@ class Menu extends Component{
         return( 
             <div className="container">
                 <div className="row">
-                    <Media list>
                         {menu}
-                    </Media>
+                </div>
+                <div className="row">
+                {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
         );
