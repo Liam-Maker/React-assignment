6f25ab5128b57a9146539c0f5356cd949214f28f
Liam-Maker <liamthompson504@gmail.com>
2021-05-13 17:09:47 -0500

    Errors resolved still trying to get pictures to load git commit -m Errors resolved still trying to get pictures to load and get navbar to not overlap

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
 import {DISHES} from '../shared/dishes';
 import DishDetail from './dishDetailComponent';
+import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -12,24 +14,27 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
-      selectedDish: null
     };
   }
 
-  onDishSelect(dishId){
-    this.setState({ 
-      selectedDish: dishId
-    });
-}
-
   render(){
+
+    const HomePage = () => {
+      return(
+        <Home />
+      )
+    }
+
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes} 
-            onClick={(dishId) => this.onDishSelect(dishId)}/>
-        <DishDetail 
-            dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish )[0]} />
+        <BrowserRouter>
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Redirect to="/home " />
+        </Switch>
+        </BrowserRouter>
         <Footer />
       </div>
        );
